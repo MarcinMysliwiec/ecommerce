bc6a2733e16821445cebcac70b20c4d32f65c149
Marcin Myśliwiec <marcin.mysliw@gmail.com>
2022-03-15 12:07:35 +0100

    card_info

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Ratings from '../components/Ratings';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-const Product = ({ product }) => {
+const Product = ({product}) => {
   return (
     <div className='card'>
       <div className='card__image'>
         <Link to={`/product/${product._id}`}>
-          <img src={product.image} alt={product.name} />
+          <img src={product.image} alt={product.name}/>
         </Link>
         {product.numInStock === 0 ? (
           <Link to={`/product/${product._id}`}>
@@ -19,14 +19,16 @@ const Product = ({ product }) => {
           </Link>
         )}
       </div>
-      <Link to={`/product/${product._id}`}>
-        <div className='card__title'>{product.name}</div>
-      </Link>
-      <div className='card__rating'>
-        <Ratings rating={product.rating} />
-      </div>
+      <div className='card__info'>
+        <Link to={`/product/${product._id}`}>
+          <div className='card__title'>{product.name}</div>
+        </Link>
+        <div className='card__rating'>
+          <Ratings rating={product.rating}/>
+        </div>
 
-      <div className='card__price'>${product.price}</div>
+        <div className='card__price'>${product.price}</div>
+      </div>
     </div>
   );
 };
